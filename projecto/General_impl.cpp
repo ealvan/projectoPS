24a8acc60850a008483e1654cbb5e1c246d8f87f
AldairBryan <asalcedoch@unsa.edu.pe>
2021-07-27 17:51:40 -0500

    Funciones que sirven para comprobar

--- projecto/General_impl.cpp
+++ projecto/General_impl.cpp
@@ -8,17 +8,25 @@ bool checkTime(string time) { //Comprueba la validez del horario(debe ser xx:xx,
 	
 	//Ingresa SOLO si el string NO esta vacio
 	if(!time.empty()){
-		for (int i=0; i < time.length(); i++) {		
+		//Recorre el time o tiempo (string)
+		for (int i=0; i < time.length(); i++) {
+			//Cuando llegue a la 3era letra		
 			if (i==2){
+				//Comprueba que tiene que ser ":"
 				if (time.at(i) != ':') {
-				return false;
+					//En caso no sea,retona falso
+					return false;
 				}
-				
+			//Cuando no sea la 3era letra
+			//Comprueba que sea mayor a 0 y menor a 9 (Un numero)	
 			}else if ( !(time.at(i) >= '0' && time.at(i) <= '9') ) {
+				//Si no lo es retorna falso
 				return false;
 			}				
 		}
+		//Cuando sea un horario
 		return true;
+	//Cuando el string esta vacio	
 	}else{ 
 		return false;
 	}
@@ -29,8 +37,11 @@ bool checkNumber(string s){ //Comprueba si el string ingresado consiste de SOLO 
 	
 	//Ingresa si el string es NO vacio
 	if(!s.empty()){
+		//Recorre la cadena ingresada
 		for (int i = 0; i < s.length(); i++){
+			//Comprueba que no este en el intervalo de la a-z y del intervalo de la A-Z
 			if ( ((s.at(i) >= 'a' && s.at(i) <= 'z') || (s.at(i) >= 'A' && s.at(i) <= 'Z') || (s.at(i) == ' ')) ){
+				//En caso contrario retorna falso
 				return false;
 			}
 		}
@@ -38,9 +49,11 @@ bool checkNumber(string s){ //Comprueba si el string ingresado consiste de SOLO 
 		//Comprueba que el string es un numero positivo
 		if ( atoi(s.c_str()) > 0 ){
 			return true;
+		//Si no lo es , retorna falso
 		}else{
 			return false;
 		}
+	//En caso el string sea vacio
 	}else{
 		return false;
 	}
@@ -49,15 +62,20 @@ bool checkNumber(string s){ //Comprueba si el string ingresado consiste de SOLO 
 }
 
 bool checkString(string s){ //Comprueba si el string consiste de solo letras
-	
+	//Ingresa si el string es NO vacio
 	if(!s.empty()){
+		//Recorre el string
 		for (int i = 0; i < s.length(); i++){
+			//Comprueba que  este en el intervalo de la a-z y del intervalo de la A-Z
 			if ( !((s.at(i) >= 'a' && s.at(i) <= 'z') || (s.at(i) >= 'A' && s.at(i) <= 'Z')) ){
+				//En caso contrario retorna falso
 				return false;
 			}
 		}
+		//Verdadero cuando sea solo letras
 		return true;
 	}
+	//Cuando este vacio
 	else{
 		return false;
 	}
@@ -67,13 +85,21 @@ bool checkString(string s){ //Comprueba si el string consiste de solo letras
 
 /* --VERIFICADOR DE VUELOS NO DIRECTOS-- */
 bool checkTime2(Time tLeaving, Time tArriving){	//Comprueba la validez de la hora de llegada y de salida
+	
+	//Verifica que la hora de salida sea mayor a la de llegada
 	if(tLeaving.hour > tArriving.hour){
+		//Retorna verdadero
 		return true;
+	//En caso sea la mismaa hora
 	}else if(tLeaving.hour == tArriving.hour){
+		//Compara los minutos
 		if(tLeaving.min > tArriving.min){
+			//Retorna verdadero
 			return true;
 		}
 	}
+
+	//Cuando no cumple
 	return false;	
 }
  
