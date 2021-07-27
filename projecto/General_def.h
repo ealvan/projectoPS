115d230ef9c8876c776f4b3f4b03950168431f2b
AldairBryan <asalcedoch@unsa.edu.pe>
2021-07-27 18:18:47 -0500

    Explicando las librerias necesarias para nuestra aplicacion

--- projecto/General_def.h
+++ projecto/General_def.h
@@ -4,7 +4,7 @@
 #include <ctype.h>
 #include <list>	 //Para las listas donde guardaremos datos
 #include <iostream>
-#include <string> // Para la conversion del atoi 
+#include <string> // Para la conversion del atoi (string a entero) 
 #include <sstream> // Para el stringstream (usado para convertirlo en string)
 #include <iomanip> // Para organizar la salida en columnas
 #include <vector>
